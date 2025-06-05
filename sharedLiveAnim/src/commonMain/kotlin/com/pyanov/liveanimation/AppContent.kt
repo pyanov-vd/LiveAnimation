ce2b219bc8f67e6cbdb33cf92448e5ca6cc28b23
pyanov <pv.android.dev@gmail.com>
2025-06-05 16:19:25 +0300

    update main screen dependencies

---
+++ sharedLiveAnim/src/commonMain/kotlin/com/pyanov/liveanimation/AppContent.kt
@@ -0,0 +1,11 @@
+package com.pyanov.liveanimation
+
+import androidx.compose.runtime.Composable
+import com.pyanov.liveanimation.designSystem.LATheme
+
+@Composable
+fun AppContent() {
+    LATheme {
+        MountainAnim()
+    }
+}
